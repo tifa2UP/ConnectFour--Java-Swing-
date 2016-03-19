49adceb9a2ded4b55e700da953d11167cde6514f
Mohammad <momosharif58@gmail.com>
2016-03-19 12:05:45 -0700

    Added import statements for java.awt and java.swing

--- src/GameView.java
+++ src/GameView.java
@@ -3,5 +3,17 @@
  * Functionality added by Mohammad on 3/19/2016
  */
 
-public class GameView {
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Displays the user interface for the game.
+ * Allows for users to play the game with just simple button clicks.
+ * @author Mohammad
+ */
+public class GameView extends JFrame{
+	
+	public static void main(String[] args){
+		
+	}
 }

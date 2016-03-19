c1caddfe79226394445bb53da27989e8c7f0ee76
Mohammad <momosharif58@gmail.com>
2016-03-19 13:21:55 -0700

    Created a StartPanel that contains a button and will soon contain an Icon. Centered it within the window.

--- src/GameView.java
+++ src/GameView.java
@@ -16,11 +16,41 @@ public class GameView extends JFrame{
 	public static void main(String[] args){
 		new GameView();
 	}
-	
+	/**
+	 * Constructs the GUI, putting together all panels and buttons.
+	 */
 	public GameView(){
 		this.setSize(600,600);
 		this.setLocationRelativeTo(null);
-		this.setVisible(true);
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		StartPanel startPanel = new StartPanel();
+		
+		this.setLayout(null);	
+		
+		this.add(startPanel);
+		
+		//Compute hidden height and width in the frame
+		int hiddenHeight = getInsets().top + getInsets().bottom;
+		int hiddenWidth = getInsets().left + getInsets().right;
+		
+		//Center the start panel in the frame
+		startPanel.setLocation((this.getWidth() - hiddenWidth - startPanel.getWidth())/2, (this.getHeight() - hiddenHeight - startPanel.getHeight())/2);
+		
+		
+		this.setVisible(true);
+	}
+	
+	/**
+	 * This is an inner class resembling the JPanel that contains the start button.
+	 * @author Momo
+	 */
+	private class StartPanel extends JPanel{
+		public StartPanel(){
+			JButton startButton = new JButton("Start Game");
+			this.add(startButton);
+			this.setSize(this.getPreferredSize());
+		}
 	}
 }

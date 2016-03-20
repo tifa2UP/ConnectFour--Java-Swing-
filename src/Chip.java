a285585e31b4719757c774aecff77957949b8250
tifa2UP <tifa3333@gmail.com>
2016-03-20 12:21:13 -0700

    Added a Chip class

---
+++ src/Chip.java
@@ -0,0 +1,36 @@
+package src;
+
+/**
+ * Created by Abdellatif on 3/20/2016.
+ */
+
+/**
+ * This class represents the chip used in the game
+ */
+public class Chip {
+    //the color of the chip
+    private String color;
+    //a unique identifier given to each type of chips
+    private int identifier;
+
+    public Chip(String color, int identifier) {
+        this.color = color;
+        this.identifier = identifier;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
+    }
+}
--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,5 +1,12 @@
+package src;
+
 /**
  * Created by Abdellatif on 3/19/2016.
  */
+
+/**
+ * This is model and the "brain" behind the connect four game
+ */
 public class ConnectFour {
+
 }
